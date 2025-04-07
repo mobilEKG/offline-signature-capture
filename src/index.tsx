eafe7ac4e83a2d26c16d9cae57de75d8ed5b7eb4
Ethan Gong <ethankgong@gmail.com>
2025-04-06 22:42:29 -0700

    Fix error TS6133: 'React' is declared but its value is never read.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";  // Tailwind base and utilities imported here
